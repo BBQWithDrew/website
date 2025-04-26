f18a10bfc235fc891e87739559def044edccf892
Thomas <108841990+BBQWithDrew@users.noreply.github.com>
2025-04-26 14:37:07 -0500

    Updates made for banner

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,7 +1,7 @@
-import React from "react";
 import { motion } from "framer-motion";
 import { Description, Hide } from "../styles";
 import { titleAnim, fade } from "../Animation";
+import { React } from "react";
 
 const AboutSection = () => {
   return (
--- src/pages/AboutBBQ.js
+++ src/pages/AboutBBQ.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 //Page Components
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
@@ -7,6 +5,7 @@ import FaqSection from "../components/FaqSection";
 import MeatCover from "../img/CoverMeat.png";
 import WoodCover from "../img/CoverWood.png";
 import drone from "../img/Drone.mp4";
+import { useEffect, useState, React } from "react";
 
 //animations
 import { motion, useScroll, useTransform } from "framer-motion";
@@ -14,7 +13,24 @@ import { pageAnimation } from "../Animation";
 import { useRef } from "react";
 
 const AboutBBQ = () => {
-  return (
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  useEffect(() => {
+    const hasSeenModal = localStorage.getItem("hasSeenModal");
+    if (!hasSeenModal) {
+      const timer = setTimeout(() => {
+        setIsModalOpen(true);
+        localStorage.setItem("hasSeenModal", "true");
+      }, 2000); // Delay of 2 seconds
+
+      return () => clearTimeout(timer); // Clear timeout if component unmounts
+    }
+  }, []);
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+  return isModalOpen ? (
+    <Modal closeModal={closeModal} />
+  ) : (
     <motion.div
       variants={pageAnimation}
       initial="hidden"
@@ -101,4 +117,27 @@ const useParalaxScroll = ({ ref }) => {
     offset: ["start start", "end start"],
   });
   return [scrollYProgress];
+};
+const Modal = ({ closeModal }) => {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center z-50">
+      <div className="bg-white bg-opacity-50 text-black rounded-lg shadow-lg p-8 max-w-md mx-auto">
+        <h2 className="text-2xl font-bold text-center mb-4">
+          Thank you for visiting our site
+        </h2>
+        <p className="text-left text-white mb-6">
+          With a growing family, we’ve decided to step away from the grill for a
+          while. We’re so grateful to everyone who has helped us grow, and we’ll
+          be back once our new grill masters are ready! Thank you so much for
+          your support and patience.
+        </p>
+        <button
+          onClick={closeModal}
+          className="bg-yellow-500 hover:bg-yellow-600 text-black font-semibold py-2 px-4 rounded-full"
+        >
+          Close
+        </button>
+      </div>
+    </div>
+  );
 };
